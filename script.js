5939e605c338ab1cf6246f8eca3747a9ded9a204
5kender <57097765+5kender@users.noreply.github.com>
2022-05-18 22:18:57 +0200

    Add font + bg color + upgrade result with username

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 const result = document.getElementById('result')
-const exemple = document.getElementById('exemple')
+const code = document.getElementById('code')
 const tag = document.getElementById('tag')
+const username = document.getElementById('username')
 const colorStart = document.getElementById('colorStart')
 const colorEnd = document.getElementById('colorEnd')
 const inputs = document.getElementsByTagName('input')
 
 const generate = () => {
-    let generatedTag = ''
-    let exempleTag = ''
+    let codeTag = ''
+    let resultTag = ''
     let nChar = tag.value.length;
 
     for (let i = 0; i < nChar; i++) {
         let hex = colorStart.value
         let tmColor = '$'
         tmColor += hex[1] + hex[3] + hex[5]
-        generatedTag += tmColor + tag.value[i]
-        exempleTag += '<span style="color: ' + hex + '">' + tag.value[i] + '</span>'
+        codeTag += tmColor + tag.value[i]
+        resultTag += '<span style="color: ' + hex + '">' + tag.value[i] + '</span>'
     }
     
-    result.innerText = generatedTag
-    exemple.innerHTML = exempleTag
+    result.innerHTML = '[' + resultTag.toUpperCase() + '] ' + username.value.toUpperCase()
+    code.innerText = codeTag
 }
+
+generate()
 
 for (let i = 0; i < inputs.length; i++) {
     inputs[i].addEventListener('input', generate)
-    
 }
