919af2481bfaf1e3f8812bccc66ada997492254a
5kender <57097765+5kender@users.noreply.github.com>
2022-05-20 23:44:30 +0200

    Add style css + remove italic

--- script.js
+++ script.js
@@ -10,40 +10,12 @@ const swapColors = document.getElementById('swapColors')
 const inputs = document.getElementsByTagName('input')
 const copyCode = document.getElementById('copyCode')
 
+const wide = document.getElementById('wide')
+const narrow = document.getElementById('narrow')
+const bold = document.getElementById('bold')
+const shadow = document.getElementById('shadow')
+
 let codeToCopy
-
-const generate = () => {
-    let codeTag = ''
-    let resultTag = ''
-    let nChar = tag.value.length;
-
-    let gradient = [colorStart.value, ...(new Gradient()
-        .setColorGradient(colorStart.value, colorEnd.value)
-        .setMidpoint(Math.abs(nChar - 1))
-        .getColors())]
-
-    for (let i = 0; i < nChar; i++) {
-        let hex = gradient[i]
-        let tmColor = '$'
-        tmColor += hex[1] + hex[3] + hex[5]
-        codeTag += tmColor + tag.value[i]
-        resultTag += '<span style="color: ' + hex + '">' + tag.value[i] + '</span>'
-    }
-
-    // TODO : transform $ to $$
-    
-    if (0 == nChar) {
-        result.innerHTML = '<br>'
-        code.innerHTML = '<br>'
-        copyCode.hidden = true
-    } else {
-        result.innerHTML = '[' + resultTag.toUpperCase() + '] ' + username.value.toUpperCase()
-        code.innerText = 'Code : ' + codeTag
-        copyCode.hidden = false
-    }
-
-    codeToCopy = codeTag
-}
 
 generate()
 
@@ -63,3 +35,62 @@ copyCode.addEventListener('click', () => {
     console.log('copy')
     navigator.clipboard.writeText(codeToCopy);
 })
+
+function generate () {
+    let codeTag = ''
+    let resultTag = ''
+    let nChar = tag.value.length;
+
+    if (wide.checked) {
+        codeTag += '$w'
+        result.classList.add('wide')
+    } else {
+        result.classList.remove('wide')
+    }
+
+    if (narrow.checked) {
+        codeTag += '$n'
+        result.classList.add('narrow')
+    } else {
+        result.classList.remove('narrow')
+    }
+
+    if (bold.checked) {
+        codeTag += '$o'
+        result.classList.add('bold')
+    } else {
+        result.classList.remove('bold')
+    }
+
+    if (shadow.checked) {
+        codeTag += '$s'
+        result.classList.add('shadow')
+    } else {
+        result.classList.remove('shadow')
+    }
+
+    let gradient = [colorStart.value, ...(new Gradient()
+        .setColorGradient(colorStart.value, colorEnd.value)
+        .setMidpoint(Math.abs(nChar - 1))
+        .getColors())]
+
+    for (let i = 0; i < nChar; i++) {
+        let hex = gradient[i]
+        codeTag += '$' + hex[1] + hex[3] + hex[5] + tag.value[i]
+        resultTag += '<span style="color: ' + hex + '">' + tag.value[i] + '</span>'
+    }
+
+    // TODO : transform $ to $$
+    
+    if (0 == nChar) {
+        result.innerHTML = '<br>'
+        code.innerHTML = '<br>'
+        copyCode.hidden = true
+    } else {
+        result.innerHTML = '[<span class="result-tag">' + resultTag.toUpperCase() + '</span>] ' + username.value.toUpperCase()
+        code.innerText = 'Code : ' + codeTag
+        copyCode.hidden = false
+    }
+
+    codeToCopy = codeTag
+}
